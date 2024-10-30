5b733f33a2f5ae803d3109c3dd232bc035939d15
nowex35 <a.hayate0726@gmail.com>
2024-10-30 13:17:31 +0900

    データ提供API完成

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
-
 urlpatterns = [
     path('upload/', views.CSVUploadView.as_view(), name='upload_csv'),
-    path('interface/', views.upload_csv_interface, name='upload_csv_interface'),
+    path('interface/', views.upload_csv_interface.as_view(), name='upload_csv_interface'),
+    path('retrieve/', views.TicketViewSet.as_view({'get':'list'}), name='retrieve_data'),
 ]
