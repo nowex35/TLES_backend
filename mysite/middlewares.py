cc91059d3fc481dcc7677f8be209bd75da75a0d2
nowex35 <a.hayate0726@gmail.com>
2024-11-18 10:05:14 +0900

    middlewaresも変更

--- mysite/middlewares.py
+++ mysite/middlewares.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 
 class SameSiteMiddleware:
     def __init__(self, get_response):
@@ -5,7 +6,6 @@ class SameSiteMiddleware:
 
     def __call__(self, request):
         response = self.get_response(request)
-        from config import settings
 
         for key in response.cookies.keys():
             response.cookies[key]['samesite'] = 'Lax' if settings.DEBUG else 'None'
